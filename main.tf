4b9a2f536224d2c38e1ca695f2430e8d6a247e76
indla28 <131199244+indla28@users.noreply.github.com>
2023-06-13 15:49:40 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
   version         = "3.56.0"
-  client_id       = "8bae677f-772b-47ac-920d-de2b1d4b2255"
+  client_id       = "5b76dc77-89e0-4da5-9775-f372c2a741d7"
   client_secret   = "BQx8Q~WbjeaPvJJODVt4XhjUH-6vaaD2ObWufbzq"
   tenant_id       = "ef89a6fd-6903-484f-bea5-f2a3f5c8cbd2"
   subscription_id = "90ea7441-2e11-46b0-b9af-4db22e508db0"
