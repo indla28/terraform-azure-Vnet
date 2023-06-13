550086329b7e5efc345e1a4129ffdef81a4afc71
indla28 <131199244+indla28@users.noreply.github.com>
2023-06-13 15:19:09 -0600

    main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+  features {}
+  version         = "3.56.0"
+  client_id       = "8bae677f-772b-47ac-920d-de2b1d4b2255"
+  client_secret   = "BQx8Q~WbjeaPvJJODVt4XhjUH-6vaaD2ObWufbzq"
+  tenant_id       = "ef89a6fd-6903-484f-bea5-f2a3f5c8cbd2"
+  subscription_id = "90ea7441-2e11-46b0-b9af-4db22e508db0"
+}
 resource "azurerm_resource_group" "vnet_rg" {
   name     = var.rgname
   location = var.location
