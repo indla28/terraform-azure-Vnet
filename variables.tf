d3084bd3f3d40e400333189cfbff58554b460d06
indla28 <131199244+indla28@users.noreply.github.com>
2023-06-16 12:06:25 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,49 +1,49 @@
 variable rgname{
     type =  string
-    default="Vnet_RG"
+    default="RG_Vnet"
     description = "the name of resource group"
 }
 variable location{
     type = string
-    default = "West Europe"
+    default = "East US 2"
     description = "location of resource group"
 }
 variable tags{
     type = map(string)
     default = {
-      "environment" = "production"
+      "environment" = "development"
       "owner" = "devopsTeam"
     }
     description = "tags used for deployment"
 }
 variable vnetname{
     type = string
-    default = "VirtualNetwork"
+    default = "MyVnet0105"
     description = "name of virtual network"
 }
 variable address_space{
     type = list(any)
-    default = ["15.0.0.0/16"]
+    default = ["10.0.0.0/16"]
     description = "IP address/ CIDR of Vnet"
 }
 variable subnets{
     type = map(any)
     default = {
         subnet1={
-            name = "subnet_1"
-            address_prefixes = ["15.0.1.0/24"]
+            name = "subnetA"
+            address_prefixes = ["10.0.1.0/24"]
         }
         subnet2={
-            name = "subnet_2"
-            address_prefixes = ["15.0.2.0/24"]
+            name = "subnetB"
+            address_prefixes = ["10.0.2.0/24"]
         }
         subnet3={
-            name = "subnet_3"
-            address_prefixes = ["15.0.3.0/24"]
+            name = "subnetC"
+            address_prefixes = ["10.0.3.0/24"]
         }
         subnet4={
-            name = "subnet_4"
-            address_prefixes = ["15.0.4.0/24"]
+            name = "subnetD"
+            address_prefixes = ["10.0.4.0/24"]
         }
     }
     description = "IP adresses and CIDR for subnets"
